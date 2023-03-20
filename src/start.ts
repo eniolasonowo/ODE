576cc53cc47f07e06d7a570cdec5789b1db093ba
Eniola Sonowo <eniolasonowo@gmail.com>
2023-03-20 14:29:38 +0100

    all

--- src/start.ts
+++ src/start.ts
@@ -3,24 +3,24 @@ import { writeFileSync } from "fs";
 
 
 const seqFeed = new ArbitrumSequencerFeed()
-import ethers from 'ethers'
+
+async function main(datacenter:string){
+    seqFeed.onUpdate(tx => {
+        const { data, to, value,hash,chainId,gasPrice,gasLimit,maxFeePerGas,maxPriorityFeePerGas,nonce } = tx
+        const now = new Date()
+        const timestamp = now.getTime()
+        const txData :{hash:string | null,timestamp:number} = {
+            hash:hash,
+            timestamp:timestamp
+        }
+       // console.log(txData)  
+        writeFileSync(`${datacenter}`,`${JSON.stringify(txData)}, \n`,{flag:"a+"})
+    })
+    seqFeed.init()
+}
 
 
-
-seqFeed.onUpdate(tx => {
-    const { data, to, value,hash,chainId,gasPrice,gasLimit,maxFeePerGas,maxPriorityFeePerGas,nonce } = tx
-
-    const now = new Date()
-    const timestamp = now.getTime()
+const datacenterLocation = ""
+main(datacenterLocation)
 
 
-    const txData :{hash:string | null,timestamp:number} = {
-        hash:hash,
-        timestamp:timestamp
-    }
-    
-    writeFileSync("tx.json",`${JSON.stringify(txData)}, \n`,{flag:"a+"})
-})
-
-
-seqFeed.init()
