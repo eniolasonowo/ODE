4459ced8ac1a4ece332ba288c321d530faf5a33c
Eniola Sonowo <eniolasonowo@gmail.com>
2023-03-31 12:50:57 +0100

    first analysis

--- src/start.ts
+++ src/start.ts
@@ -5,16 +5,19 @@ import { writeFileSync } from "fs";
 const seqFeed = new ArbitrumSequencerFeed()
 
 async function main(datacenter:string){
-    seqFeed.onUpdate(tx => {
-        const { data, to, value,hash,chainId,gasPrice,gasLimit,maxFeePerGas,maxPriorityFeePerGas,nonce } = tx
+    seqFeed.onUpdate(async(tx )=> {
+        const { data, to, value,hash,chainId,gasPrice,gasLimit,maxFeePerGas,maxPriorityFeePerGas,nonce,type,typeName } = tx
         const now = new Date()
         const timestamp = now.getTime()
         const txData :{hash:string | null,timestamp:number} = {
             hash:hash,
             timestamp:timestamp
         }
-       // console.log(txData)  
-        writeFileSync(`${datacenter}`,`${JSON.stringify(txData)}, \n`,{flag:"a+"})
+        if(to === "0xa3Fb9C1B0355F5b736BE139332eac8E4F6030356"){
+
+            console.log(hash,type,typeName,maxPriorityFeePerGas,maxFeePerGas,)  
+        }
+      //  writeFileSync(`${datacenter}`,`${JSON.stringify(txData)}, \n`,{flag:"a+"})
     })
     seqFeed.init()
 }
@@ -25,7 +28,7 @@ const datacenterLocation = ""
 try{
     main(datacenterLocation)
 }catch(err:any){
-    console.log(err)
+    //console.log(err)
 }
 
 
